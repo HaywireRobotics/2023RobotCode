d65c2bc1c1041f7633e55b340b3066c0ce410404
Jacob <steljaco@isu.edu>
2023-03-28 21:30:18 -0600

    Important Comments

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -2,6 +2,12 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+/*
+ * Hail Mary, full of grace, the Lord is with thee. Blessed are thou among women,
+ * and blessed is the fruit of thy womb, Jesus. Holy Mary, Mother of God, pray for us sinners,
+ * now and at the hour of our death. Amen.
+ */
+
 package frc.robot;
 
 import edu.wpi.first.wpilibj.RobotBase;
